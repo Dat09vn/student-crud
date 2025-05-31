630ae4b3594ecfdeeb8d9b05185fde20d69849d6
dat09 <tiendat14031998@gmail.com>
2025-05-31 19:17:38 +0700

    change from mysql to postgress

--- database/db.go
+++ database/db.go
@@ -1,25 +1,24 @@
 package database
 
 import (
-	"fmt"
 	"log"
 	"os"
+
 	"student-crud/models"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	dsn := os.Getenv("DB_DSN") // example: "user:password@tcp(host:port)/dbname?parseTime=true"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := os.Getenv("DB_DSN")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to database: ", err)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	db.AutoMigrate(&models.Student{})
 	DB = db
-	fmt.Println("Database connected!")
 }
