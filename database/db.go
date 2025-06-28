9ddc8eb2114bfc096350788bd8b51b089a7f3a77
dat09 <tiendat14031998@gmail.com>
2025-06-28 17:47:39 +0700

    Add .env file to connect to DB Add .env to gitignore

--- database/db.go
+++ database/db.go
@@ -6,6 +6,7 @@ import (
 
 	"student-crud/models"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -13,6 +14,13 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the database connection string from environment variable
 	dsn := os.Getenv("DB_DSN")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
